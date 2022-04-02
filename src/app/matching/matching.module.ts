98aa2c0d7ee66dbd322e02e4ac03cbb9ae76a63a
sesp13 <sespinosarte99@gmail.com>
2022-04-02 00:07:54 -0500

    add material autocomplete in match form

--- src/app/matching/matching.module.ts
+++ src/app/matching/matching.module.ts
@@ -3,13 +3,24 @@ import { CommonModule } from '@angular/common';
 
 // Material
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
 
 import { MatchingRoutingModule } from './matching-routing.module';
 
 import { MatchComponent } from './pages/match/match.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [MatchComponent],
-  imports: [CommonModule, MatAutocompleteModule, MatchingRoutingModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatchingRoutingModule,
+  ],
 })
 export class MatchingModule {}
--- src/app/matching/pages/match/match.component.ts
+++ src/app/matching/pages/match/match.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-match',
   templateUrl: './match.component.html',
-  styleUrls: ['./match.component.css']
+  styleUrls: ['./match.component.css'],
 })
 export class MatchComponent implements OnInit {
+  myForm: FormGroup = this.fb.group({});
+  myControl = new FormControl();
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions: Observable<string[]>;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map((value) => this._filter(value))
+    );
   }
 
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter((option) =>
+      option.toLowerCase().includes(filterValue)
+    );
+  }
 }
