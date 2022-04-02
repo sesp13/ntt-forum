e4c5183b02657ce26d3adb702a6206259c619869
sesp13 <sespinosarte99@gmail.com>
2022-04-01 23:44:58 -0500

    add angular material

--- src/app/matching/matching.module.ts
+++ src/app/matching/matching.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// Material
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+
 import { MatchingRoutingModule } from './matching-routing.module';
 
 import { MatchComponent } from './pages/match/match.component';
 
 @NgModule({
   declarations: [MatchComponent],
-  imports: [CommonModule, MatchingRoutingModule],
+  imports: [CommonModule, MatAutocompleteModule, MatchingRoutingModule],
 })
 export class MatchingModule {}
